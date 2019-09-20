755b6ae1de18162e4bf8fa8d1adfc4e531c17045
weffology <stephlarkinrose@gmail.com>
2019-09-19 21:13:57 -0400

    beginning of pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    //global variables
+    var zipCode = 0;
+    var difficultyLevel = "";
+    var queryURLTrail = "";
+    var queryURLWeather = "";
+
+    //create on click function for the submit button
+    //create a zipcode validation function
+    //create a function to convert zipcode to long lat
+    //
+}
