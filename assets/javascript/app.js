3d52569e9601c7caab42e31898260458e6b7ea71
weffology <stephlarkinrose@gmail.com>
2019-09-21 11:56:45 -0400

    more HTML for weather column

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,74 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     //global variables
-   var zipCode = 0;
-   var difficultyLevel = "";
-   var queryURLTrail = "";
-   var weatherAPIKey = "4f16b57711b05e07200b836df723d031";
-   var cityName = "";
-   var tempHigh = "";
-   var tempLow = "";
-   var clouds = "";
-   var precip = "";
+    var zipCode = 0;
+    var difficultyLevel = "";
+    var queryURLTrail = "";
+    var weatherAPIKey = "4f16b57711b05e07200b836df723d031";
+    var cityName = "";
+    var tempHigh = "";
+    var tempLow = "";
+    var clouds = "";
+    var precip = "";
 
     //create on click function for the submit button
-   $("#submitBtn").on("click", function () {
-    zipCode = $("#inputZip").val();
-    difficultyLevel = $("#difficultyLevel").val();
-    // IsValidZipCode();
-    getWeather();
-    console.log(zipCode);
-    console.log(difficultyLevel);
-})
-//create a zipcode validation function
-// function IsValidZipCode(zipCode) {
-//     var isValid = /^[0-9]{5}(?:-[0-9]{4})?$/.test(zipCode);
-//     if (!isValid) {
-//         alert("This is not Valid");
-//         return false;
-//     }
-//     else {
-//         console.log("Zip Code is valid")
-//     }
-// }
-function getWeather() {
-    var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&units=imperial&appid=" + weatherAPIKey;
-    $.ajax({
-        url: queryURLWeather,
-        method: "GET"
+    $("#submitBtn").on("click", function () {
+        zipCode = $("#inputZip").val();
+        difficultyLevel = $("#difficultyLevel").val();
+        // IsValidZipCode();
+        getWeather();
+        console.log(zipCode);
+        console.log(difficultyLevel);
     })
-        .then(function (response) {
-            console.log(response);
-            cityName = response.name;
-            tempHigh = response.main.temp_max;
-            tempLow = response.main.temp_min;
-            clouds = response.clouds;
-            console.log(cityName);
-            console.log(tempHigh);
-            console.log(tempLow);
-            console.log(clouds);
-            returnResults();
-
+    //create a zipcode validation function
+    // function IsValidZipCode(zipCode) {
+    //     var isValid = /^[0-9]{5}(?:-[0-9]{4})?$/.test(zipCode);
+    //     if (!isValid) {
+    //         alert("This is not Valid");
+    //         return false;
+    //     }
+    //     else {
+    //         console.log("Zip Code is valid")
+    //     }
+    // }
+    function getWeather() {
+        var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&units=imperial&appid=" + weatherAPIKey;
+        $.ajax({
+            url: queryURLWeather,
+            method: "GET"
         })
-}
+            .then(function (response) {
+                console.log(response);
+                cityName = response.name;
+                tempHigh = response.main.temp_max;
+                tempLow = response.main.temp_min;
+                clouds = response.clouds;
+                console.log(cityName);
+                console.log(tempHigh);
+                console.log(tempLow);
+                console.log(clouds);
+                returnResults();
 
-    
+            })
+    }
+
+
     //create a function to push values
     function returnResults() {
+        
+        //here's all the build out for the trails column
         var divTrails = $("<div>");
-            divTrails.attr("class", "col-sm-8");
-            divTrails.attr("id", "trailResults");
-            divTrails.html("You've chosen a " + difficultyLevel + " trail.");
+        divTrails.attr("class", "col-sm-8");
+        divTrails.attr("id", "trailResults");
+        divTrails.html("You've chosen a " + difficultyLevel + " trail.");
+        
+        //here's the build out for the weather column
         var divWeather = $("<div>");
-            divWeather.attr("class", "col-sm-4");
-            divWeather.attr("id", "weatherResults");
-            divWeather.html("Here is your current weather forcast for: " + cityName);
-            divWeather.append("Your high temperature is: " + tempHigh);
+        divWeather.attr("class", "col-sm-4");
+        divWeather.attr("id", "weatherResults");
+        divWeather.html("<p>Here is your current weather forcast for: " + "<strong>" + cityName + "</strong>" + "</p>");
+        divWeather.append("<p>Your high temperature for today is: " + "<strong>" + tempHigh + "</strong>" + "</p>");
+        divWeather.append("<p>Your low temperature for today is: " + "<strong>" + tempLow + "</strong>" + "</p>");
         $("#row").html(divTrails);
         $("#row").append(divWeather);
     }
