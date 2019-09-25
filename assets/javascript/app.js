2c52b12bbf6b736a4b720f5c37bee20a5cfac74c
weffology <stephlarkinrose@gmail.com>
2019-09-24 20:15:09 -0400

    more functionality added, still need to display missing values as 'not available' or something instead of null

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     //global variables
     var zipCode = 0;
     var difficultyLevel = "";
-    var trailAPIKey = "200596650-c553e831ecdd1148b80c200473482707";
+    var trailAPIKey = "200596653-a7e0e793ed563b44b0305ea2b62726b3";
     var weatherAPIKey = "4f16b57711b05e07200b836df723d031";
     var cityName = "";
     var tempHigh = "";
@@ -25,8 +25,6 @@ $(document).ready(function () {
         // IsValidZipCode();
         getWeather();
         getLongitude();
-        console.log(zipCode);
-        console.log(difficultyLevel);
     })
 
     function getWeather() {
@@ -36,45 +34,37 @@ $(document).ready(function () {
             method: "GET"
         })
             .then(function (response) {
-                console.log(response);
                 cityName = response.name;
                 tempHigh = response.main.temp_max;
                 tempLow = response.main.temp_min;
                 clouds = response.clouds;
-                console.log(cityName);
-                console.log(tempHigh);
-                console.log(tempLow);
-                console.log(clouds);
-                returnTrails();
                 returnWeather();
 
             })
     }
 
     function getLongitude() {
-        var longitudeAPIKey = "Stukr8nGoCSSLDp6J5k8KmMKdI2DRgcHoYPU7UhSGMzaN6duoDJBXC96GT09AWFo";
+        var longitudeAPIKey = "ZLj7GLdIDUTjJ0OWlaMIsYkXKMgRTJ0bZ4itlMOfDbtX9jrFBcQEpe9p6GrkMnRp";
         var queryURLLongitude = "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/" + longitudeAPIKey + "/info.json/" + zipCode + "/degrees";
         $.ajax({
             url: queryURLLongitude,
             method: "GET"
         }).then(function (lResponse) {
-            console.log('got zip')
             longitude = lResponse.lng;
             latitude = lResponse.lat;
-            console.log("longitude: " + longitude);
-            console.log("latitude: " + latitude);
             getTrails();
         })
     }
 
     function getTrails() {
-        var queryURLTrail = "https://cors-anywhere.herokuapp.com/www.hikingproject.com/data/get-trails?lat= " + latitude + "&lon=" + longitude + "&maxDistance=10&key=" + trailAPIKey;
+        console.log("sdfsdfsdfv");
+        var queryURLTrail = "https://cors-anywhere.herokuapp.com/www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=10&key=" + trailAPIKey;
         $.ajax({
             url: queryURLTrail,
             method: "GET",
         })
             .then(function (response) {
-                console.log(response);
+                console.log(response.trails.length);
                 for (var i = 0; i < response.trails.length; i++) {
                     trailName = response.trails[i].name;
                     trailSummary = response.trails[i].summary;
@@ -82,44 +72,36 @@ $(document).ready(function () {
                     trailDistance = response.trails[i].length;
                     trailStatus = response.trails[i].conditionStatus;
                     trailDetails = response.trails[i].conditionDetails;
-                    console.log(trailName);
                     returnTrails();
                 }
 
             })
     }
 
-    //create a function to push values to the trails section
-    function returnTrails() {
-
-        //here's all the build out for the trails column
-        var divTrails = $("<div>");
-        divTrails.attr("class", "col-sm-8");
-        divTrails.attr("id", "trailResults");
-        divTrails.html("You've chosen a " + difficultyLevel + " trail.");
-        divTrails.append("<p><img src=" + trailImage + "></p>");
-        divTrails.append("<p>Trail Name: " + "<strong>" + trailName + "</strong>" + "</p>");
-        divTrails.append("<p>Trail Summary: " + "<strong>" + trailSummary + "</strong>" + "</p>");
-        divTrails.append("<p>Trail Distance: " + "<strong>" + trailDistance + "</strong>" + "</p>");
-        divTrails.append("<p>Trail Status: " + "<strong>" + trailStatus + "</strong>" + "</p>");
-        divTrails.append("<p>Trail Details: " + "<strong>" + trailDetails + "</strong>" + "</p>");
-        //push all the above data into the row
-        $("#row").html(divTrails);
-    }
-
     //create a function to push values to the weather section
     function returnWeather() {
-
-        //here's the build out for the weather column
         var divWeather = $("<div>");
-        divWeather.attr("class", "col-sm-4");
+        divWeather.attr("class", "col-sm-12");
         divWeather.attr("id", "weatherResults");
         divWeather.html("<p>Here is your current weather forcast for: " + "<strong>" + cityName + "</strong>" + "</p>");
         divWeather.append("<p>Your high temperature for today is: " + "<strong>" + tempHigh + "</strong>" + "</p>");
         divWeather.append("<p>Your low temperature for today is: " + "<strong>" + tempLow + "</strong>" + "</p>");
-        //push all the above data into the row
-        $("#row").append(divWeather);
+        $("#row1").html(divWeather);
     }
+
+        //create a function to push values to the trails section
+        function returnTrails() {
+            var divTrails = $("<div>");
+            divTrails.attr("class", "col-lg-12");
+            divTrails.attr("id", "trailResults");
+            divTrails.html("<p><img src=" + trailImage + "></p>");
+            divTrails.append("<p>Trail Name: " + "<strong>" + trailName + "</strong>" + "</p>");
+            divTrails.append("<p>Trail Summary: " + "<strong>" + trailSummary + "</strong>" + "</p>");
+            divTrails.append("<p>Trail Distance: " + "<strong>" + trailDistance + "</strong>" + "</p>");
+            divTrails.append("<p>Trail Status: " + "<strong>" + trailStatus + "</strong>" + "</p>");
+            divTrails.append("<p>Trail Details: " + "<strong>" + trailDetails + "</strong>" + "</p>");
+            $("#row2").append(divTrails);
+        }
 
 })
 
